82b4b24cb07aa44738987d8de8911e592f8b4825
minxiang <294315568@qq.com>
2019-01-04 17:37:40 +0800

    update

--- boot/src/main/java/cn/beerate/dao/Impl/AdminDaoImpl.java
+++ boot/src/main/java/cn/beerate/dao/Impl/AdminDaoImpl.java
@@ -2,8 +2,10 @@ package cn.beerate.dao.Impl;
 
 import cn.beerate.model.entity.t_admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminDaoImpl extends JpaRepository<t_admin,Long> {
 
-    t_admin findByAccountAndAndPassword(String account, String password);
+    t_admin findByAccountAndPassword(String account, String password);
 }
