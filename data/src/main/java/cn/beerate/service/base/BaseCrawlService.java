5eeb761d3fa1249be7b590f77efacf193fc64727
minxiang <294315568@qq.com>
2019-01-08 17:46:00 +0800

    修改该类的返回类型

--- data/src/main/java/cn/beerate/service/base/BaseCrawlService.java
+++ data/src/main/java/cn/beerate/service/base/BaseCrawlService.java
@@ -1,5 +1,6 @@
 package cn.beerate.service.base;
 
+import cn.beerate.common.Message;
 import cn.beerate.common.util.Crawler;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.logging.Log;
@@ -14,17 +15,20 @@ public class BaseCrawlService {
 
     private static Log log  = LogFactory.getLog(IntegrationService.class);
 
-    public String crawl(String url,Map<String,String> params){
+    public Message<String> crawl(String url, Map<String,String> params){
         String result =  Crawler.getInstance().getString(url,params);
-        log.info(result);
+
+        log.debug(result);
 
         //判断股票代码是否正确
         JSONObject jsonObject = JSONObject.parseObject(result);
         if(!StringUtil.isBlank(jsonObject.getString("status"))){
-            return null;
+            Message<String> message = Message.error("股票代码不合法");
+            message.setData(result);
+            return message;
         }
 
-        return result;
+        return Message.success(result);
     }
 
 
