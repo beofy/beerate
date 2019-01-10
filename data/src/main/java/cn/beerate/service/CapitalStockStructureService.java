1aa8a24be034044caa6031cf8d0ca37944813b89
minxiang <294315568@qq.com>
2019-01-10 16:55:38 +0800

    股本结构

---
+++ data/src/main/java/cn/beerate/service/CapitalStockStructureService.java
@@ -0,0 +1,32 @@
+package cn.beerate.service;
+
+import cn.beerate.common.Message;
+import cn.beerate.service.base.BaseCrawlService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 股本结构
+ */
+@Component
+public class CapitalStockStructureService extends BaseCrawlService {
+    private Log log = LogFactory.getLog(CapitalStockStructureService.class);
+    private final static String CAPITAL_STOCK_STRUCTURE_AJAX="http://emweb.securities.eastmoney.com/CapitalStockStructure/CapitalStockStructureAjax";
+
+
+    /**
+     * 股本结构
+     * @param code 股票代码
+     * @return
+     */
+    public Message<String> crawlCapitalStockStructure(String code){
+        Map<String,String> params = new HashMap<String,String>();
+        params.put("code",code);
+
+        return super.crawl(this.CAPITAL_STOCK_STRUCTURE_AJAX,params);
+    }
+}
