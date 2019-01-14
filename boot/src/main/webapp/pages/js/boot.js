e3241ee95e32999947a197fb96855a0a5da9bc00
minxiang <294315568@qq.com>
2019-01-14 10:43:15 +0800

    static resource

---
+++ boot/src/main/webapp/pages/js/boot.js
@@ -0,0 +1,14 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    //baseUrl: '',
+    paths: {
+        "jquery":'lib/jquery',
+        "layui":"layui/layui",
+        "encrypt":"encrypt/encrypt",//rsa工具
+        "3DES":"encrypt/3DES"
+    },
+    shim:{
+
+    }
+});
+
