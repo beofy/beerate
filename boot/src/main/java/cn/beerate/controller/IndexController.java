189960637da496eda6d367b7e6fe41b9d7015d21
minxiang <294315568@qq.com>
2019-01-14 17:18:27 +0800

    update

--- boot/src/main/java/cn/beerate/controller/IndexController.java
+++ boot/src/main/java/cn/beerate/controller/IndexController.java
@@ -4,24 +4,22 @@ package cn.beerate.controller;
 import cn.beerate.common.Message;
 import cn.beerate.model.entity.t_admin;
 import cn.beerate.service.AdminService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private AdminService adminService;
-
     @GetMapping(value = "/")
     @ResponseBody
-    public Message login(HttpServletResponse response){
-        Message<t_admin> message =Message.success(adminService.loggin("123","123"));
-        message.setMsg("响应成功");
-        return message;
+    public Message login(){
+        return Message.ok("响应成功");
     }
 }
