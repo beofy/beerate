8dd9ce9efe6f9185c1012251ddda95b360a9b349
minxiang <294315568@qq.com>
2019-01-15 18:17:46 +0800

    添加状态码

--- common/src/main/java/cn/beerate/common/Message.java
+++ common/src/main/java/cn/beerate/common/Message.java
@@ -61,11 +61,33 @@ public class Message<T> {
         this.data = data;
     }
 
+    @Override
+    public String toString() {
+        return "Message{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
     public class Code{
 
-        public final static int SUCCESS = 200;
+        /** 公共成功码200:处理成功 */
+        public static final int SUCCESS = 200;
 
-        public final static int ERROR = -1;
+        /** 公共错误码 -1:处理失败 */
+        public static final int ERROR = -1;
 
+        /** 公共错误码-404:未找到资源() */
+        public static final int ERROR_404 = -404;
+
+        /** 公共错误码-500:程序错误 */
+        public static final int ERROR_500 = -500;
+
+        /** 拦截标识-100:登录拦截 */
+        public static final int NOT_LOGIN = -100;
+
+        /** 登录超时*/
+        public static final int  LOGIN_TIME_OUT = -102;
     }
 }
