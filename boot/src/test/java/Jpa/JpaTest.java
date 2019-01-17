dc9485991457d1c66916086ef24af6ab5ee94f79
minxiang <294315568@qq.com>
2019-01-17 16:20:25 +0800

    update

--- boot/src/test/java/Jpa/JpaTest.java
+++ boot/src/test/java/Jpa/JpaTest.java
@@ -16,26 +16,15 @@ import java.util.Date;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/applicationContext.xml")
 public class JpaTest {
-
     @Autowired
     private AdminDaoImpl adminDao;
 
-
-
-    @PersistenceContext
-    private EntityManager em;
-
     @Test
     public void testJpaAdd(){
-        if(em.getTransaction().isActive()){
-            em.getTransaction().begin();
-        }
-
         t_admin admin =  new t_admin();
         admin.setCreateTime(new Date());
         admin.setAccount("asd");
         admin.setPassword("123456");
         adminDao.save(admin);
-        em.getTransaction().commit();
     }
 }
