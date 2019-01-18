d4ee4277fbefd6c514431b6b45d658047ee77328
minxiang <294315568@qq.com>
2019-01-18 15:59:28 +0800

    添加swagger2 文档api框架

---
+++ common/src/main/java/cn/beerate/config/Swagger2Config.java
@@ -0,0 +1,53 @@
+package cn.beerate.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration     //让Spring来加载该类配置
+@EnableWebMvc      //启用Mvc，非springboot框架需要引入注解@EnableWebMvc
+@EnableSwagger2    //启用Swagger2
+public class Swagger2Config extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars*")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                //扫描指定包中的swagger注解
+                //.apis(RequestHandlerSelectors.basePackage("com.xia.controller"))
+                //扫描所有有注解的api，用这种方式更灵活
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("巴雷特接口文档")
+                .description("文档描述~~")
+                .termsOfServiceUrl("termsOfServiceUrl实例")
+                .contact("contact实例")
+                .version("1.0.0")
+                .build();
+    }
+}
