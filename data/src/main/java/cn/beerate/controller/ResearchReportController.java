86e673b2214a700f97ad3b82e6046efa1a2b14a4
minxiang <294315568@qq.com>
2019-01-23 17:30:31 +0800

    研究报告接口

---
+++ data/src/main/java/cn/beerate/controller/ResearchReportController.java
@@ -0,0 +1,51 @@
+package cn.beerate.controller;
+
+import cn.beerate.common.Message;
+import cn.beerate.common.util.StockCodeUtil;
+import cn.beerate.model.bean.ResearchReport;
+import cn.beerate.model.bean.companybignews.Gqzy;
+import cn.beerate.service.ResearchReportService;
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.*;
+import org.jsoup.helper.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(description = "研究报告")
+@RestController
+@ApiResponses({
+        @ApiResponse(code = Message.Code.SUCCESS,message = "success"),
+        @ApiResponse(code = Message.Code.ERROR,message = "error"),
+})
+public class ResearchReportController {
+
+
+    @Autowired
+    private ResearchReportService researchReportService;
+
+    @GetMapping(value = "/researchReport")
+    @ApiOperation(value = "根据股票代码获取研究报告", notes = "包含(研报摘要 公司研报 行业研报)")
+    @ApiImplicitParams({
+            @ApiImplicitParam(value = "股票代码", name = "code", paramType = "query", dataType = "string", required = true),
+    })
+    public Message<ResearchReport> researchReport(@Param(value = "code")String code){
+        String aBStock = StockCodeUtil.getABStock(code);
+        if(aBStock==null){
+            return Message.error("股票代码错误");
+        }
+
+        Message<String> message = researchReportService.researchReport(aBStock);
+        //未抓取到
+        if(message.getCode()==Message.Code.ERROR){
+            return Message.error("无数据");
+        }
+
+        //json转对象
+        JSONObject jsonObject = JSONObject.parseObject(message.getData());
+
+        return Message.success(jsonObject.toJavaObject(ResearchReport.class));
+    }
+
+}
---
+++ data/src/main/java/cn/beerate/model/bean/researchreport/Ybzy.java
@@ -0,0 +1,33 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package cn.beerate.model.bean.researchreport;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Auto-generated: 2019-01-23 17:3:41
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@ApiModel(description = "研报摘要")
+@Data
+public class Ybzy {
+    @ApiModelProperty(value = "等级")
+    private String grade;
+
+    @ApiModelProperty(value = "时间")
+    private String date;
+
+    @ApiModelProperty(value = "链接地址：/report/{date}/{linkid}.html")
+    private String linkid;
+
+    @ApiModelProperty(value = "标题")
+    private String title;
+
+    @ApiModelProperty(value = "内容")
+    private String content;
+}
