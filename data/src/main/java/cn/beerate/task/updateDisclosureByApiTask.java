db4fc2bdfa8f42c9137060dacf1d9460f77e1679
minxiang <294315568@qq.com>
2019-02-26 13:43:29 +0800

    添加定时抓取公告，每天0 6 12 18 点执行

--- data/src/main/java/cn/beerate/task/updateDisclosureByApiTask.java
+++ data/src/main/java/cn/beerate/task/updateDisclosureByApiTask.java
@@ -1,18 +1,28 @@
 package cn.beerate.task;
 
+import cn.beerate.service.base.BaseCrawlService;
 import cn.beerate.service.cninfo.DisclosureService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component("updateDisclosureByApiTask")
 public class updateDisclosureByApiTask {
+    private static final Log logger  = LogFactory.getLog(updateDisclosureByApiTask.class);
+
     @Autowired
     private DisclosureService disclosureService;
 
-    @Scheduled(cron = "0 55 11 * * ? ")
+    /**
+     * 每天0 6 12 18 点执行
+     */
+    @Scheduled(cron = "0 0 0,6,12,18 * * ? ")
     public void doUpdateDisclosureByApi(){
+        logger.info("------------------开始定时任务：更新巨潮公告数据------------------");
         disclosureService.updateDisclosureByApi();
+        logger.info("------------------结束定时任务：更新巨潮公告数据------------------");
     }
 
 }
