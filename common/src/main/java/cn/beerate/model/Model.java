15d992d7a0ae7d6b9476349b9070924f791d04de
minxiang <294315568@qq.com>
2019-03-07 11:10:51 +0800

    Model类修改

--- common/src/main/java/cn/beerate/model/Model.java
+++ common/src/main/java/cn/beerate/model/Model.java
@@ -1,5 +1,8 @@
 package cn.beerate.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -7,28 +10,13 @@ import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @MappedSuperclass
+@Setter
+@Getter
 public class Model {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    /** 主键 */
     private Long id;
 
-    /** 生成时间 */
     private Date createTime = new Date();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
