fb55c21c93ddf1d82147f9a47275b2f8fb2d09d1
minxiang <294315568@qq.com>
2019-03-11 17:04:13 +0800

    添加定时

--- data/src/main/java/cn/beerate/task/UpdateStockDataTask.java
+++ data/src/main/java/cn/beerate/task/UpdateStockDataTask.java
@@ -1,33 +1,35 @@
 package cn.beerate.task;
 
-import cn.beerate.controller.CompanySurveyController;
 import cn.beerate.service.cninfo.DisclosureService;
 import cn.beerate.service.eastmoney.f10.CompanyBigNewsService;
+import cn.beerate.service.eastmoney.f10.CompanyManagementService;
 import cn.beerate.service.eastmoney.f10.CompanySurveyService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component()
-public class updateDisclosureByApiTask {
-    private static final Log logger  = LogFactory.getLog(updateDisclosureByApiTask.class);
+@Component
+public class UpdateStockDataTask {
+    private static final Log logger  = LogFactory.getLog(UpdateStockDataTask.class);
 
     private DisclosureService disclosureService;
     private CompanySurveyService companySurveyService;
     private CompanyBigNewsService companyBigNewsService;
-    public updateDisclosureByApiTask(DisclosureService disclosureService, CompanySurveyService companySurveyService) {
+    private CompanyManagementService companyManagementService;
+
+    public UpdateStockDataTask(DisclosureService disclosureService, CompanySurveyService companySurveyService, CompanyBigNewsService companyBigNewsService, CompanyManagementService companyManagementService) {
         this.disclosureService = disclosureService;
         this.companySurveyService = companySurveyService;
+        this.companyBigNewsService = companyBigNewsService;
+        this.companyManagementService = companyManagementService;
     }
-
 
     /**
      * 每天0开始，每8小时执行一次,更新巨潮公告数据
      */
     @Scheduled(cron = "0 0 0/8 * * ?")
-    public void doUpdateDisclosureByApi(){
+    public void updateDisclosure(){
         logger.info("------------------开始定时任务：更新巨潮公告数据------------------");
         disclosureService.updateDisclosureByApi();
         logger.info("------------------结束定时任务：更新巨潮公告数据------------------");
@@ -44,10 +46,17 @@ public class updateDisclosureByApiTask {
     }
 
     @Scheduled(cron = "0 30 10 * * ?")
-    public void companyBigNewsService(){
+    public void updateCompanyBigNews(){
         logger.info("------------------开始定时任务：更新公司大事数据------------------");
         companyBigNewsService.updateAllStockCodesData();
         logger.info("------------------结束定时任务：更新公司大事数据------------------");
+    }
+
+    @Scheduled(cron = "0 0 11 * * ?")
+    public void updateCompanyManagement(){
+        logger.info("------------------开始定时任务：更新公司高管数据------------------");
+        companyManagementService.updateAllStockCodesData();
+        logger.info("------------------结束定时任务：更新公司高管数据------------------");
     }
 
 }
