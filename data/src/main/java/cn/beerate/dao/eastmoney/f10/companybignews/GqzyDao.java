9e6d1163aa6682ff80eaa94b2013a99669ef9a61
minxiang <294315568@qq.com>
2019-03-11 16:08:46 +0800

    公司大事dao类

---
+++ data/src/main/java/cn/beerate/dao/eastmoney/f10/companybignews/GqzyDao.java
@@ -0,0 +1,10 @@
+package cn.beerate.dao.eastmoney.f10.companybignews;
+
+import cn.beerate.dao.IBaseDao;
+import cn.beerate.model.entity.eastmoney.f10.companybignews.t_eastmoney_gqzy;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GqzyDao extends IBaseDao<t_eastmoney_gqzy> {
+
+}
---
+++ data/src/main/java/cn/beerate/dao/eastmoney/f10/companybignews/SdgdcgbdDao.java
@@ -0,0 +1,9 @@
+package cn.beerate.dao.eastmoney.f10.companybignews;
+
+import cn.beerate.dao.IBaseDao;
+import cn.beerate.model.entity.eastmoney.f10.companybignews.t_eastmoney_sdgdcgbd;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SdgdcgbdDao extends IBaseDao<t_eastmoney_sdgdcgbd> {
+}
